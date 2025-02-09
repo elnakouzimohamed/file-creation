5b643cc299049fc9e5a6b1a24c83ec27b74a7149
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-09 13:43:14 +0200

    Change

---
+++ questions.py
@@ -0,0 +1,270 @@
+form_data = {
+    "Form1": {
+        "What is the name?": "{Name}",
+        "What is the age?": "{age}",
+        "What is the department?": "{department}"
+    },
+    "Form2": {
+        "Date of session": "{{date_session}}",
+        "Location of Session": "{{location}}",
+        "Who referred/identified the person": {
+            "{{Check_1}}": "Referral from another organization",
+            "{{Check_2}}": "Family, neighbor or community member",
+            "{{Check_3}}": "Internal referral",
+            "{{Check_4}}": "Self-referral",
+            "{{Check_5}}": "Community volunteer/focal point",
+            "{{specify_id}}": "Other (specify)"
+        },
+        "Date of identification": "{{date_id}}",
+        "Do you need any support to take part in this meeting?": {
+            "{{Check_7}}": "Yes",
+            "{{Check_8}}": "No"
+        },
+        "Note down support required and what has been put in place": "{{support_details}}",
+        "Case Code": "{{case_code}}",
+        "Date of Birth": "{{date_of_birth}}",
+        "Gender": {
+            "{{Check_9}}": "Male",
+            "{{Check_10}}": "Female",
+            "{{Check_11}}": "Other"
+        },
+        "Do you have difficulty seeing, even if wearing glasses?": {
+            "{{Check_12}}": "No, no difficulty",
+            "{{Check_13}}": "Yes, some difficulty",
+            "{{Check_14}}": "Yes, a lot of difficulty",
+            "{{Check_15}}": "Cannot do it at all",
+            "{{Check_16}}": "Refused"
+        },
+        "Do you have difficulty hearing, even if using a hearing aid?": {
+            "{{Check_17}}": "No, no difficulty",
+            "{{Check_18}}": "Yes, some difficulty",
+            "{{Check_19}}": "Yes, a lot of difficulty",
+            "{{Check_20}}": "Cannot do it at all",
+            "{{Check_21}}": "Refused"
+        },
+        "Do you have difficulty walking or climbing steps?": {
+            "{{Check_22}}": "No, no difficulty",
+            "{{Check_23}}": "Yes, some difficulty",
+            "{{Check_24}}": "Yes, a lot of difficulty",
+            "{{Check_25}}": "Cannot do it at all",
+            "{{Check_26}}": "Refused"
+        },
+        "Do you have difficulty remembering or concentrating?": {
+            "{{Check_27}}": "No, no difficulty",
+            "{{Check_28}}": "Yes, some difficulty",
+            "{{Check_29}}": "Yes, a lot of difficulty",
+            "{{Check_30}}": "Cannot do it at all",
+            "{{Check_31}}": "Refused"
+        },
+        "Do you have difficulty washing all over or dressing?": {
+            "{{Check_32}}": "No, no difficulty",
+            "{{Check_33}}": "Yes, some difficulty",
+            "{{Check_34}}": "Yes, a lot of difficulty",
+            "{{Check_35}}": "Cannot do it at all",
+            "{{Check_36}}": "Refused"
+        },
+        "Do you have difficulty communicating, for example understanding or being understood?": {
+            "{{Check_37}}": "No, no difficulty",
+            "{{Check_38}}": "Yes, some difficulty",
+            "{{Check_39}}": "Yes, a lot of difficulty",
+            "{{Check_40}}": "Cannot do it at all",
+            "{{Check_41}}": "Refused"
+        },
+        "Disability status (case worker only)": {
+            "{{Check_42}}": "Yes",
+            "{{Check_43}}": "No"
+        },
+        "What is your citizenship/country of origin?": {
+            "{{Check_44}}": "Syrian",
+            "{{Check_45}}": "Lebanese",
+            "{{Check_46}}": "PRS",
+            "{{Check_47}}": "PRL",
+            "{{Check_48}}": "Stateless",
+            "{{Check_49}}": "Other, specify"
+        },
+        "Displacement status": {
+            "{{Check_50}}": "IDP",
+            "{{Check_51}}": "Refugee",
+            "{{Check_52}}": "Host community",
+            "{{Check_53}}": "Other, specify"
+        },
+        "What is your civil/marital status?": {
+            "{{Check_54}}": "Single",
+            "{{Check_55}}": "Married",
+            "{{Check_56}}": "Divorced/separated",
+            "{{Check_57}}": "Widowed"
+        },
+        "Are you registered with the UNHCR?": {
+            "{{Check_58}}": "Yes",
+            "{{Check_59}}": "No"
+        },
+        "Please specify the reasons": "{{reasons}}",
+        "Do you have caregiver/s?": {
+            "{{Check_60}}": "Yes",
+            "{{Check_61}}": "No"
+        },
+        "What is the name of the caregiver/s?": "{{caregiver_name}}",
+        "What is your relationship to them?": "{{caregiver_relationship}}",
+        "Can you tell me what brought you here today?": "{{reason_of_coming}}",
+        "Can you explain whether you have any immediate medical, physical or emotional needs?": {
+            "{{Check_62}}": "Yes",
+            "{{needs}}": "Briefly explain the needs",
+            "{{Check_63}}": "No"
+        },
+        "Do you feel safe within your community and/or the location you currently live?": {
+            "{{Check_64}}": "Yes, feeling safe",
+            "{{Check_65}}": "No, feeling unsafe"
+        },
+        "Explain what makes you feel safe or unsafe": "{{explain_1}}",
+        "Do you feel safe in your home?": {
+            "{{Check_66}}": "Yes, feeling safe",
+            "{{Check_67}}": "No, feeling unsafe"
+        },
+        "Explain what makes you feel safe or unsafe (home)": "{{explain_2}}",
+        "Can you tell me about any people or groups that are particularly significant in your life?": "{{significant_people}}",
+        "Do you feel that some of these people or groups are able to support you?": {
+            "{{Check_68}}": "Yes",
+            "{{Check_69}}": "No"
+        },
+        "Explain": "{{explain_3}}",
+        "Do you participate in and contribute to decisions that affect you in the home and community?": {
+            "{{Check_70}}": "Yes",
+            "{{Check_71}}": "No"
+        },
+        "Explain participation in decision-making": "{{explain_4}}",
+        "Do you have access to available services on an equal basis with others?": {
+            "{{Check_72}}": "Yes",
+            "{{Check_73}}": "No"
+        },
+        "Explain access to services": "{{explain_5}}",
+        "Can you summarize the key concerns that worry you the most?": "{{key_concerns}}",
+        "What steps have you taken to try to resolve these concerns?": "{{resolve_concerns}}",
+        "What worked well and what did not?": "{{what_worked_and_not}}",
+        "Do you or other members of the family have work or an income of any kind at the moment?": {
+            "{{Check_74}}": "Yes",
+            "{{Check_75}}": "No"
+        },
+        "Explain work or income situation": "{{explain_6}}",
+        "Vulnerability Profile": {
+            "{{Check_109}}": "Person with Disability",
+            "{{Check_110}}": "Older person at-risk",
+            "{{Check_111}}": "Woman at-risk"
+        },
+        "Risk-Level determination": {
+            "{{Check_112}}": "No Risk",
+            "{{Check_113}}": "Low Risk",
+            "{{Check_114}}": "Medium Risk",
+            "{{Check_115}}": "High Risk"
+        }
+    }
+
+
+,
+    
+    "Form3": {
+        "{{staff_code}}": "Staff Code",
+        "{{case_code}}": "Case Code",
+        "{{date}}": "Date Assessment",
+        "Type of Assessment": {
+            "{{Check_1}}": "Home Visit",
+            "{{Check_2}}": "Phone",
+            "{{Check_3}}": "Community Development Centre",
+            "{{Check_4}}": "Mobile Safe Space",
+            "{{Check_5}}": "Other",
+            "{{assessment_type}}": "Please specify if other"
+        },
+        "Do you need any support to take part in this meeting?": {
+            "{{Check_6}}": "Yes",
+            "{{Check_7}}": "No"
+        },
+        "{{support_required}}": "Support required if yes",
+        "Is everyone in your household registered with UNHCR?": {
+            "{{Check_8}}": "Yes",
+            "{{Check_9}}": "No",
+            "{{Check_10}}": "Don't know"
+        },
+        "Do all of your children have official birth certificates?": {
+            "{{Check_11}}": "Yes",
+            "{{Check_12}}": "No",
+            "{{Check_13}}": "Don't know"
+        },
+        "Are you aware of how to register your child’s birth in Lebanon?": {
+            "{{Check_14}}": "Yes",
+            "{{Check_15}}": "No",
+            "{{Check_16}}": "Don't know"
+        },
+        "Do you have a valid residence permit/residency card?": {
+            "{{Check_17}}": "Yes",
+            "{{Check_18}}": "No",
+            "{{Check_19}}": "Don't know"
+        },
+        "Does anyone else in your household have a valid residence permit/residency card?": {
+            "{{Check_20}}": "Yes",
+            "{{Check_21}}": "No",
+            "{{Check_22}}": "Don't know"
+        },
+        "Do/did you or your family face any safety or security threat or incidents?": {
+            "{{Check_23}}": "Yes",
+            "{{explain_1}}": "Explain if yes",
+            "{{Check_24}}": "No"
+        },
+        "Who are you living with?": {
+            "{{Check_25}}": "Immediate family",
+            "{{Check_26}}": "Relatives",
+            "{{Check_27}}": "Non-relatives",
+            "{{Check_28}}": "Independently",
+            "{{Check_29}}": "Other"
+        },
+        "{{number_of_people_living_with}}": "How many people are you living with?",
+        "Where are your immediate family members (if not with family)?": {
+            "{{Check_30}}": "In another country",
+            "{{Check_31}}": "In another location in this country",
+            "{{Check_32}}": "Don’t know",
+            "{{explain_2}}": "Other"
+        },
+        "{{reunification}}": "Are you in need of family tracing and reunification support?",
+        "What is your housing situation?": {
+            "{{Check_34}}": "Owned (Apartment or House)",
+            "{{Check_35}}": "Rented",
+            "{{rental_amount}}": "Rental amount",
+            "{{Check_36}}": "Informal tenure/unpaid",
+            "{{Check_37}}": "Living with host family (paying rent)",
+            "{{family_rental_amount}}": "Family rental amount",
+            "{{Check_38}}": "Living with host family (not paying rent)",
+            "{{Check_39}}": "Camp or informal settlement",
+            "{{Check_40}}": "Other",
+            "{{explain_3}}": "Specify if other"
+        },
+        "Do you have a written lease agreement?": {
+            "{{Check_41}}": "Yes",
+            "{{Check_42}}": "No"
+        },
+        "{{explain_4}}": "Explain lease agreement",
+        "Do you feel safe/secure that you can stay in this shelter as long as you want to?": {
+            "{{Check_43}}": "Very Stable",
+            "{{Check_44}}": "Stable",
+            "{{Check_45}}": "Insecure",
+            "{{Check_46}}": "Very Insecure",
+            "{{secure_details}}": "If insecure or very insecure, provide further details"
+        },
+        "How would you assess the accessibility of your place?": {
+            "{{Check_47}}": "Accessible",
+            "{{Check_48}}": "Limited accessibility - Needs Rehabilitation",
+            "{{Check_49}}": "Not accessible at all - Needs Rehabilitation"
+        },
+        "What arrangements do you need to make your place more accessible?": {
+            "{{Check_50}}": "Hand rails",
+            "{{Check_51}}": "Ramps at the entrance",
+            "{{Check_52}}": "Ramps at the doorstep",
+            "{{Check_53}}": "Install Bathroom and toilet aids",
+            "{{Check_54}}": "Install external electric Elevator",
+            "{{Check_55}}": "Other",
+            "{{explain_5}}": "Specify if other"
+        },
+        "{{housing_situation}}": "Comment/observation on housing situation"
+    }
+
+,
+    "Form4": {"Name": "Emily Johnson", "Age": 22, "Department": "Finance"},
+    "Form5": {"Name": "Michael Lee", "Age": 35, "Department": "IT"},
+}
